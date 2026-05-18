582f8ae6a3d11e2fc6c6a5b8969af38d61310d99
nicolemorais <nicolemoraisatt@gmail.com>
2026-05-18 20:07:45 -0300

    feat: implementa listagem de tarefas com paginação e ordenação

--- taskmaster-api/src/main/java/taskmaster_api/service/TaskService.java
+++ taskmaster-api/src/main/java/taskmaster_api/service/TaskService.java
@@ -4,6 +4,8 @@ import taskmaster_api.model.Task;
 import taskmaster_api.dto.TaskRequestDTO;
 import taskmaster_api.dto.TaskResponseDTO;
 import taskmaster_api.repository.TaskRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,5 +27,15 @@ public class TaskService {
 
         Task tarefaSalva = taskRepository.save(novaTarefa);
         return new TaskResponseDTO(tarefaSalva);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<TaskResponseDTO> listarTarefas(Pageable pageable) {
+        Page<Task> tarefasPage = taskRepository.findAll(pageable);
+
+
+
+        
+        return tarefasPage.map(TaskResponseDTO::new);
     }
 }
